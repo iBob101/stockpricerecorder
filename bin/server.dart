5b084fcc77de2bf83de0d59d6d01be8b4c82c8df
iBob101 <michael.s.humphreys@gmail.com>
2016-07-23 23:22:54 +0100

    Working on server

--- bin/server.dart
+++ bin/server.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
+import 'dart:convert';
 
 main() async {
 
-  var portEnv = Platform.environment['PORT'];
-  var port = portEnv == null ? 9999 : int.parse(portEnv);
+  var port = int.parse(Platform.environment['PORT']);
 
   var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, port);
   print("Serving at ${server.address}:${server.port}");
@@ -14,4 +14,16 @@ main() async {
       ..write('Hello, bobworld')
       ..close();
   }
+
+  server.listen((HttpRequest request) {
+    var resp = JSON.encode({
+      'Dart on Heroku': true,
+      'Buildpack URL': 'https://github.com/igrigorik/heroku-buildpack-dart',
+      'Environment': Platform.environment}
+    );
+    request.response..headers.set(HttpHeaders.CONTENT_TYPE, 'application/json')
+      ..write(resp)
+      ..close();
+  });
+
 }
