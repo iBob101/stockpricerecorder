5b66a97833f7713017aecb38d046d0ede11b1be5
iBob101 <michael.s.humphreys@gmail.com>
2016-07-24 00:29:49 +0100

    Working on server

--- bin/server.dart
+++ bin/server.dart
@@ -1,31 +1,26 @@
 import 'dart:io';
-import 'dart:convert' show JSON;
+import 'package:http_server/http_server.dart';
 
 main() async {
 
-  var port = int.parse(Platform.environment['PORT']);
+  String msg;
 
   HttpServer server = await HttpServer.bind('0.0.0.0', port);
   print("Serving at ${server.address}:${server.port}");
 
   await for (var request in server) {
-    print("Request receved type 1");
+
+    HttpClient client = new HttpClient();
+    String url = 'http://www.bloomberg.com/markets/chart/data/1D/AAPL:US';
+    client.getUrl(Uri.parse(url)).then((HttpClientRequest request) {
+      return request.close();
+    }).then(HttpBodyHandler.processResponse).then((HttpClientResponseBody body) {
+      msg = body.body;
+    });
     request.response
       ..headers.contentType = new ContentType("text", "plain", charset: "utf-8")
-      ..write('Hello, bobworld')
+      ..write(msg)
       ..close();
   }
-
-  server.listen((HttpRequest request) {
-    print("Request received type 2");
-    var resp = JSON.encode({
-      'Dart on Heroku': true,
-      'Buildpack URL': 'https://github.com/igrigorik/heroku-buildpack-dart',
-      'Environment': Platform.environment}
-    );
-    request.response..headers.set(HttpHeaders.CONTENT_TYPE, 'application/json')
-      ..write(resp)
-      ..close();
-  });
 
 }
