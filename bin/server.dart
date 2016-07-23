78dafd8e8fba8563eb7f7f1297572cafd94abc45
iBob101 <michael.s.humphreys@gmail.com>
2016-07-23 23:11:33 +0100

    Add dart_to_js_script_rewriter

--- bin/server.dart
+++ bin/server.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 
 main() async {
-  var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
+
+  var portEnv = Platform.environment['PORT'];
+  var port = portEnv == null ? 9999 : int.parse(portEnv);
+
+  var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, port);
   print("Serving at ${server.address}:${server.port}");
 
   await for (var request in server) {
